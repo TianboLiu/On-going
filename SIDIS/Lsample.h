6c7b50aea515dadac0f4e83834e8f092c75ad68d
TianboLiu <liutianbo1987@gmail.com>
2016-06-26 20:10:25 -0400

    Update Lsample.h

--- SIDIS/Lsample.h
+++ SIDIS/Lsample.h
@@ -32,10 +32,15 @@ class Lsample{
   double _A[_NMAX_];
   int _PSD;//parameter space dimension
   double _hessian[11][11];
+  double _eigenvectors[11][11];
+  double _eigenvalues[11];
   double _para0[11];
+  double _central[11];
  public:
   Lsample(int Atype);
-  int SetParameters(const double * para);
+  int InitialA1para();
+  int InitialA2para();
+  int InitialA3para();
   int GetNeutronPlusData(const char * datafile);
   int GetNeutronMinusData(const char * datafile);
   int GetProtonPlusData(const char * datafile);
@@ -52,4 +57,76 @@ Lsample::Lsample(int Atype){
     std::cout << "Lsample initialize error: invalid asymmetry type!" << std::endl;
 }
 
+int Lsample::InitialA1para(){
+  //Sivers
+  _para[0] = 0.35;//Nu
+  _para[1] = -0.90;//Nd
+  _para[2] = -0.24;//Ns
+  _para[3] = 0.04;//Nubar
+  _para[4] = -0.40;//Ndbar
+  _para[5] = 1.0;//Nsbar
+  _para[6] = 0.73;//au
+  _para[7] = 1.08;//ad
+  _para[8] = 0.79;//asea
+  _para[9] = 3.46;//b
+  _para[10] = 0.34;//MS2
+  //central
+  _central[0] = _para[0];//Nu
+  _central[1] = _para[1];//Nd
+  _central[2] = _para[3];//Nubar
+  _central[3] = _para[4];//Ndbar
+  _central[4] = _para[6];//au
+  _central[5] = _para[7];//ad
+  _central[6] = _para[8];//asea
+  _central[7] = _para[9];//b
+  _central[8] = _para[10];//MS2
+  return 0;
+}
+
+int Lsample::InitialA2para(){
+  //transversity
+  _para[0] = 0.46;//Nu
+  _para[1] = -1.0;//Nd
+  _para[2] = 1.11;//a
+  _para[3] = 3.64;//b
+  _para[4] = 0.25;//kt2
+  //Collins
+  _para[5] = 0.49;//Nfav
+  _para[6] = -1.0;//Ndis
+  _para[7] = 1.06;//c
+  _para[8] = 0.07;//d
+  _para[9] = 1.5;//MC2
+  //central
+  _central[0] = _para[0];//Nu
+  _central[1] = _para[1];//Nd
+  _central[2] = _para[2];//a
+  _central[3] = _para[3];//b
+  _central[4] = _para[5];//Nfav
+  _central[5] = _para[6];//Ndis
+  _central[6] = _para[7];//c
+  _central[7] = _para[8];//d
+  _central[8] = _para[9];//MC2
+  return 0;
+}
+
+int Lsample::InitialA3para(){
+  //pretzelosity
+  _para[0] = 1.0;//Nu
+  _para[1] = -1.0;//Nd
+  _para[2] = 2.5;//a
+  _para[3] = 2.0;//b
+  _para[4] = 0.18;//Mh2
+  //Collins
+  _para[5] = 0.49;//Nfav
+  _para[6] = -1.0;//Ndis
+  _para[7] = 1.06;//c
+  _para[8] = 0.07;//d
+  _para[9] = 1.5;//MC2
+  //central
+  _central[0] = _para[0];//Nu
+  _central[1] = _para[1];//Nd
+  _central[2] = _para[2];//a
+  _central[3] = _para[4];//Mh2
+  return 0;
+}
 
