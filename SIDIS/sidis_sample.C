fc79364128c1ba590aa0fe1bf4cd3ae010d458e7
TianboLiu <liutianbo1987@gmail.com>
2016-06-29 02:15:27 -0400

    Update sidis_sample.C

--- SIDIS/sidis_sample.C
+++ SIDIS/sidis_sample.C
@@ -6,9 +6,10 @@ int main(){
   Lsample a2(2);
   a2.GetData("Neutron/solidneutron.root", 2);
   a2.GetData("Proton/solidproton.root", 2);
-  a2.PrintData(23);
+  //a2.PrintData(23);
   a2.InitialA2para();
-  cout << a2.Chi2A2(0) << endl;
+  cout << a2.Chi2A2(1) << endl;
+  a2.SetHessianA2(1);
   
   return 0;
 }
