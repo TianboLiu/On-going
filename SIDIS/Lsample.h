8f84f0cc6c14bcd9baca2332bc2cf0fbd76aba4c
TianboLiu <liutianbo1987@gmail.com>
2016-06-29 03:51:26 -0400

    Add files via upload

--- SIDIS/Lsample.h
+++ SIDIS/Lsample.h
@@ -292,7 +292,10 @@ int Lsample::LikeliSampleA2(int calls, const char * savefile){
   TFile * fs = new TFile(savefile, "RECREATE");
   TTree * Ts = new TTree("fitpara", "fitpara");
   Ts->SetDirectory(fs);
-  double as[9], bs[9];
+  TMatrix as(9, 1);
+  TMatrix bs(9, 1);
+  TMatrix smatrix(9, 9);
+  TMatrix ev(9, 9);
   double para[9];
   double weight, bound, chi2;
   double kt2 = 0.25;
@@ -315,22 +318,26 @@ int Lsample::LikeliSampleA2(int calls, const char * savefile){
   Ts->Branch("bound", &bound, "bound/D");
   double cup;
   gRandom->SetSeed(0);
+  for (int i = 0; i < 9; i++){
+    ev(i, i) = _eigenvalues[i];
+    for (int j = 0; j < 9; j++){
+      smatrix(i, j) = _smatrix[i][j];
+    }
+  }
   for (Long64_t n = 0; n < calls; n++){
     std::cout << "#" << n << std::endl;
     cup = 0;
     for (int j = 0; j < 9; j++){
-      bs[j] = gRandom->Gaus(0.0, 1.0/sqrt(_eigenvalues[j]));
-      std::cout << bs[j] << std::endl;
-      as[j] = 0.0;
-      cup = cup + bs[j]*bs[j]*_eigenvalues[j]/2.0;
+      bs(j,1) = gRandom->Gaus(0.0, 1.0/sqrt(ev(j,j)) );
+      cup = cup + bs(j,1)*bs(j,1)*ev(j,j)/2.0;
     }
+    bs.Print();
+    as = smatrix * bs;
+    as.Print();
+    as = smatrix * bs;
+    as.Print();
     for (int i = 0; i < 9; i++){
-      for(int j = 0; j < 9; j++){
-	as[i] = as[i] + _smatrix[i][j] * bs[j];
-      }
-    }
-    for (int i = 0; i < 9; i++){
-      para[i] = _central[i] + as[i];
+      para[i] = _central[i] + as(i, 1);
     }
     if (std::abs(para[0]) > 1.0 || std::abs(para[1]) > 1.0) bound = 0;
     else if (std::abs(para[4]) > 1.0 || std::abs(para[5]) > 1.0) bound = 0;
