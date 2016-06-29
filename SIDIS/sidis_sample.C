1faffa1b1da27981b5b0b772d933e05043ec5ca7
TianboLiu <liutianbo1987@gmail.com>
2016-06-29 14:47:00 -0400

    Add files via upload

--- SIDIS/sidis_sample.C
+++ SIDIS/sidis_sample.C
@@ -3,14 +3,19 @@
 using namespace std;
 
 int main(){
-  Lsample a2(2);
-  a2.GetData("Neutron/solidneutron.root", 2);
-  a2.GetData("Proton/solidproton.root", 2);
-  //a2.PrintData(23);
-  a2.InitialA2para();
-  cout << a2.Chi2A2(1) << endl;
-  a2.SetHessianA2(1);
-  a2.LikeliSampleA2(100, "test.root");
-  
+  if (false){
+    Lsample a2(2);
+    a2.GetData("Neutron/solidneutron.root", 2);
+    a2.GetData("Proton/solidproton.root", 2);
+    a2.InitialA2para();
+    a2.UniformSampleA2(1, 100000.0, "Paraset/collinssample1.root");
+    a2.UniformSampleA2(0, 100000.0, "Paraset/collinssample0.root");
+  }
+  if (true){
+    Lsample re(2);
+    cout << re.FindChi2Limit("Paraset/collinssample0.root", 0.955, 0) << endl;
+    cout << re.FindChi2Limit("Paraset/collinssample1.root", 0.955, 1) << endl;
+  }
+
   return 0;
 }
