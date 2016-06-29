c03bbdf872a160091bedc7671b928b2ecfd3278e
TianboLiu <liutianbo1987@gmail.com>
2016-06-29 14:46:45 -0400

    Add files via upload

--- SIDIS/Lsample.h
+++ SIDIS/Lsample.h
@@ -7,6 +7,7 @@
 #include "TRandom.h"
 #include "TRandom3.h"
 #include "TMath.h"
+#include "TH1D.h"
 #include "TString.h"
 #include "TVectorD.h"
 #include "TMatrixD.h"
@@ -49,9 +50,13 @@ class Lsample{
   double SetHessianA1(const int err);
   double SetHessianA2(const int err);
   double SetHessianA3(const int err);
-  int LikeliSampleA1(int calls, const char * savefile);
-  int LikeliSampleA2(int calls, const char * savefile);
-  int LikeliSampleA3(int calls, const char * savefile);
+  int LikeliSampleA1(const int _err, const int calls, const char * savefile);
+  int LikeliSampleA2(const int _err, const int calls, const char * savefile);
+  int LikeliSampleA3(const int _err, const int calls, const char * savefile);
+  int UniformSampleA1(const int _err, const int calls, const char * savefile);
+  int UniformSampleA2(const int _err, const int calls, const char * savefile);
+  int UniformSampleA3(const int _err, const int calls, const char * savefile);
+  double FindChi2Limit(const char * paraset, const double CL, const int cut);
 };
 
 Lsample::Lsample(int Atype){
@@ -170,18 +175,22 @@ int Lsample::GetData(const char * datafile, const int Atype){
   Td->SetBranchAddress("E1rel", &Erel[0]);
   Td->SetBranchAddress("E2rel", &Erel[1]);
   Td->SetBranchAddress("E3rel", &Erel[2]);
+  int skip = 0;
   for (int i = 0; i < ndata; i++){
     Td->GetEntry(i);
-    _Nucleon[_Ndata + i] = Nucleon;
-    _Hadron[_Ndata + i] = Hadron;
+    _Nucleon[_Ndata + i - skip] = Nucleon;
+    _Hadron[_Ndata + i - skip] = Hadron;
     for (int j = 0; j < 5; j++){
-      _kin[_Ndata + i][j] = kin[j];
+      _kin[_Ndata + i - skip][j] = kin[j];
     }
-    _A0[_Ndata + i] = Asym[Atype - 1];
-    _E0[_Ndata + i] = Estat[Atype - 1];
-    _ET[_Ndata + i] = sqrt(Estat[Atype - 1] * Estat[Atype - 1] + Eabs[Atype - 1] * Eabs[Atype - 1] + Erel[Atype - 1] * Erel[Atype - 1] * Asym[Atype - 1] * Asym[Atype - 1]);
+    _A0[_Ndata + i - skip] = Asym[Atype - 1];
+    _E0[_Ndata + i - skip] = Estat[Atype - 1];
+    _ET[_Ndata + i - skip] = sqrt(Estat[Atype - 1] * Estat[Atype - 1] + Eabs[Atype - 1] * Eabs[Atype - 1] + Erel[Atype - 1] * Erel[Atype - 1] * Asym[Atype - 1] * Asym[Atype - 1]);
+    if (_ET[_Ndata + i - skip] < 1.0e5) continue;
+    else skip++;
   }
-  _Ndata = _Ndata + ndata;
+  _Ndata = _Ndata + ndata - skip;
+  std::cout << "skip: " << skip << std::endl;
   return 0;
 }
 
@@ -245,7 +254,7 @@ double Lsample::Chi2A2(const int err = 1, const double * fitpara = 0){
       sum = sum + pow( (_A[i] - _A0[i]) / _E0[i], 2);
     }
     else {
-      if (_ET[i] < 1.0e5) sum = sum + pow( (_A[i] - _A0[i]) / _ET[i], 2);
+      sum = sum + pow( (_A[i] - _A0[i]) / _ET[i], 2);
     }
   }
   return sum;
@@ -253,25 +262,28 @@ double Lsample::Chi2A2(const int err = 1, const double * fitpara = 0){
 
 double Lsample::SetHessianA2(const int err){
   _err = err;
-  double da = 1.0e-8;
-  double db = 1.0e-8;
+  double da = 1.0e-6;
+  double db = 1.0e-6;
   double chi2min = Chi2A2(err);
+  std::cout << "chi2min: " << chi2min << std::endl;
   double newpara[9];
   double chi2[4];
   TMatrixD hessian(9,9);
+  double scale[9] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+  //double scale[9] = {5.0e-3, 1.0e-2, 1.0e-2, 5.0e-2, 5.0e-3, 1.0e-2, 1.0e-2, 2.0e-3, 1.0e-2};
   for (int i = 0; i < 9; i++){
     for (int j = 0; j < 9; j++){
       for (int s = 0; s < 9; s++) newpara[s] = _central[s];
-      newpara[i] = newpara[i] + da;
-      newpara[j] = newpara[j] + db;
+      newpara[i] = newpara[i] + da * scale[i];
+      newpara[j] = newpara[j] + db * scale[j];
       chi2[0] = Chi2A2(err, newpara);//a+da, b+db
-      newpara[j] = newpara[j] - 2.0 * db;
+      newpara[j] = newpara[j] - 2.0 * db * scale[j];
       chi2[1] = Chi2A2(err, newpara);//a+da, b-db
-      newpara[i] = newpara[i] - 2.0 * da;
+      newpara[i] = newpara[i] - 2.0 * da * scale[i];
       chi2[3] = Chi2A2(err, newpara);//a-da, b-db
-      newpara[j] = newpara[j] + 2.0 * db;
+      newpara[j] = newpara[j] + 2.0 * db * scale[j];
       chi2[2] = Chi2A2(err, newpara);//a-da, b+db
-      if (i == j) hessian(i, i) = (chi2[0] + chi2[3] - 2.0 * chi2min) / (2.0 * pow(da+db, 2));
+      if (i == j) hessian(i, i) = (chi2[0] + chi2[3] - 2.0 * chi2min) / (2.0 * pow((da+db), 2));
       else hessian(i, j) = (chi2[0] + chi2[3] - chi2[1] - chi2[2]) / (8.0 * da * db);
       _hessian[i][j] = hessian(i, j);
     }
@@ -285,10 +297,15 @@ double Lsample::SetHessianA2(const int err){
       _smatrix[i][j] = smatrix(i, j);
     }
   }
+  /* hessian.Print(); */
+  /* ev.Print(); */
+  /* smatrix.Print(); */
+  /* std::cout << hessian.Determinant() << std::endl; */
   return 0;
 }
 
-int Lsample::LikeliSampleA2(int calls, const char * savefile){
+int Lsample::LikeliSampleA2(const int err, const int calls, const char * savefile){
+  _err = err;
   TFile * fs = new TFile(savefile, "RECREATE");
   TTree * Ts = new TTree("fitpara", "fitpara");
   Ts->SetDirectory(fs);
@@ -317,7 +334,7 @@ int Lsample::LikeliSampleA2(int calls, const char * savefile){
   Ts->Branch("weight", &weight, "weight/D");
   Ts->Branch("bound", &bound, "bound/D");
   double cup;
-  gRandom->SetSeed(0);
+  //gRandom->SetSeed(0);
   for (int i = 0; i < 9; i++){
     ev(i, i) = _eigenvalues[i];
     for (int j = 0; j < 9; j++){
@@ -350,7 +367,92 @@ int Lsample::LikeliSampleA2(int calls, const char * savefile){
   return 0;
 }
     
-    
+int Lsample::UniformSampleA2(const int err, const int calls, const char * savefile){
+  _err = err;
+  TFile * fs = new TFile(savefile, "RECREATE");
+  TTree * Ts = new TTree("fitpara", "fitpara");
+  Ts->SetDirectory(fs);
+  double para[9];
+  double shift[9];
+  double width[9] = {0.025, 0.05, 0.025, 0.11, 0.03, 0.048, 0.055, 0.07, 0.06};
+  double weight, bound, chi2;
+  double kt2 = 0.25;
+  Ts->Branch("Nu", &para[0], "Nu/D");
+  Ts->Branch("Nd", &para[1], "Nd/D");
+  Ts->Branch("au", &para[2], "au/D");
+  Ts->Branch("ad", &para[2], "ad/D");
+  Ts->Branch("bu", &para[3], "bu/D");
+  Ts->Branch("bd", &para[3], "bd/D");
+  Ts->Branch("kt2", &kt2, "kt2/D");
+  Ts->Branch("Nfav", &para[4], "Nfav/D");
+  Ts->Branch("Ndis", &para[5], "Ndis/D");
+  Ts->Branch("cu", &para[6], "cu/D");
+  Ts->Branch("cd", &para[6], "cd/D");
+  Ts->Branch("du", &para[7], "du/D");
+  Ts->Branch("dd", &para[7], "dd/D");
+  Ts->Branch("MC2", &para[8], "MC2/D");
+  Ts->Branch("Chi2", &chi2, "Chi2/D");
+  Ts->Branch("weight", &weight, "weight/D");
+  Ts->Branch("bound", &bound, "bound/D");
+  double cup;
+  gRandom->SetSeed(0);  
+  for (Long64_t n = 0; n < calls; n++){
+    if (n%100 == 0) std::cout << "#" << n << std::endl;
+    shift[0] = gRandom->Gaus(0.0, width[0]);
+    shift[1] = gRandom->Gaus(0.0, width[1]);
+    shift[2] = gRandom->Gaus(0.0, width[2]);
+    shift[3] = gRandom->Gaus(0.0, width[3]);
+    shift[4] = gRandom->Gaus(0.0, width[4]);
+    shift[5] = gRandom->Gaus(0.0, width[5]);
+    shift[6] = gRandom->Gaus(0.0, width[6]);
+    shift[7] = gRandom->Gaus(0.0, width[7]);
+    shift[8] = gRandom->Gaus(0.0, width[8]);
+    cup = 
+      (shift[0]*shift[0]/width[0]/width[0] 
+      + shift[1]*shift[1]/width[1]/width[1]
+      + shift[2]*shift[2]/width[2]/width[2]
+      + shift[3]*shift[3]/width[3]/width[3]
+      + shift[4]*shift[4]/width[4]/width[4]
+      + shift[5]*shift[5]/width[5]/width[5]
+      + shift[6]*shift[6]/width[6]/width[6]
+      + shift[7]*shift[7]/width[7]/width[7]
+       + shift[8]*shift[8]/width[8]/width[8]) / 2.0;
+    for (int i = 0; i < 9; i++){
+      para[i] = _central[i] + shift[i];
+    }
+    if (para[7] < 0.0) continue;
+    if (std::abs(para[0]) > 1.0 || std::abs(para[1]) > 1.0) bound = 0;
+    else if (std::abs(para[4]) > 1.0 || std::abs(para[5]) > 1.0) bound = 0;
+    else bound = 1;
+    chi2 = Chi2A2(_err, para);
+    weight = exp(cup - chi2/2.0);
+    Ts->Fill();
+  }
+  fs->Write();
+  return 0;
+} 
   
-  
+double Lsample::FindChi2Limit(const char * paraset, const double CL, const int cut){
+  TFile * fr = new TFile(paraset, "r");
+  TTree * Tr = (TTree *) fr->Get("fitpara");
+  TH1D * h0 = new TH1D("h0", "h0", 1, 0.0, 10000.0);
+  TH1D * h1 = new TH1D("h1", "h1", 1000, 0.0, 20.0);
+  if (cut == 0){
+    Tr->Project("h0", "Chi2", "weight*(1.>0)");
+    Tr->Project("h1", "Chi2", "weight*(1.>0)");
+  }
+  else {
+    Tr->Project("h0", "Chi2", "weight*(bound>0.1)");
+    Tr->Project("h1", "Chi2", "weight*(bound>0.1)");
+  }
+  double sum = h0->Integral(1, -1);
+  double upper = 0;
+  for (int i = 1; i <= 1000; i++){
+    if (h1->Integral(1, i) / sum < CL) continue;
+    upper = h1->GetBinCenter(i);
+    break;
+  }
+  return upper;
+}
+
 #endif
